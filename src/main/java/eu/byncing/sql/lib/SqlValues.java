9f3029e2492b2f57ed0681b958ad74913581d9e2
byncing <byncing@gmail.com>
2021-11-20 23:33:11 +0100

    Values convert UUID to String

--- src/main/java/eu/byncing/sql/lib/SqlValues.java
+++ src/main/java/eu/byncing/sql/lib/SqlValues.java
@@ -1,14 +1,28 @@
 package eu.byncing.sql.lib;
 
+import java.util.UUID;
+
 public class SqlValues {
 
-    private final Object[] values;
+    private final Object[] array;
 
     public SqlValues(Object... values) {
-        this.values = values;
+        for (int i = 0; i < values.length; i++) {
+            Object value = values[i];
+            if (value instanceof UUID) values[i] = String.valueOf(value);
+        }
+        this.array = values;
     }
 
-    public Object[] getValues() {
-        return values;
+    public int length() {
+        return array.length;
+    }
+
+    public Object[] getArray() {
+        return array;
+    }
+
+    public Object getArray(int index) {
+        return array[index];
     }
 }
