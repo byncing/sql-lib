759e1f1199304140da67f7b470cbaaa643b64110
byncing <byncing@gmail.com>
2021-11-20 23:34:28 +0100

    Clean code

--- src/main/java/eu/byncing/sql/lib/SqlKeys.java
+++ src/main/java/eu/byncing/sql/lib/SqlKeys.java
@@ -2,27 +2,35 @@ package eu.byncing.sql.lib;
 
 public class SqlKeys {
 
-    private final String[] keys;
+    private final String[] array;
 
     public SqlKeys(String... keys) {
-        this.keys = keys;
+        this.array = keys;
     }
 
-    public String[] getKeys() {
-        return keys;
+    public int length() {
+        return array.length;
+    }
+
+    public String[] getArray() {
+        return array;
+    }
+
+    public String getArray(int index) {
+        return array[index];
     }
 
     public String toValues() {
         String values = toString();
-        for (String key : keys) values = values.replace(key, "?");
+        for (String key : array) values = values.replace(key, "?");
         return values;
     }
 
     public String toSet() {
         StringBuilder builder = new StringBuilder();
-        for (int i = 0; i < keys.length; i++) {
-            builder.append(keys[i]).append(" = ").append("?");
-            if (i != keys.length - 1) builder.append(", ");
+        for (int i = 0; i < array.length; i++) {
+            builder.append(array[i]).append(" = ").append("?");
+            if (i != array.length - 1) builder.append(", ");
         }
         return builder.toString();
     }
@@ -34,9 +42,9 @@ public class SqlKeys {
     @Override
     public String toString() {
         StringBuilder builder = new StringBuilder();
-        for (int i = 0; i < keys.length; i++) {
-            builder.append(keys[i]);
-            if (i != keys.length - 1) builder.append(", ");
+        for (int i = 0; i < array.length; i++) {
+            builder.append(array[i]);
+            if (i != array.length - 1) builder.append(", ");
         }
         return builder.toString();
     }
